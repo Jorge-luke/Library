c88424c2d04c577f9584a8c579605d006ce67788
jorge-luke <jorgestl123@gmail.com>
2025-01-31 17:46:39 -0400

    finish project

--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
 let library = [];
 
 function Book(title, author, pages, read){
-    this.title = title,
-    this.author = author,
-    this.pages = pages
-    this.read = read
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
 function addBookToLibrary (title, author, pages, read){
@@ -12,8 +12,31 @@ function addBookToLibrary (title, author, pages, read){
    library.push(newBook);
 }
 
-addBookToLibrary('harry potter', "jk rolling", 350, "yes");
-addBookToLibrary('Eragon', "asdasdasd", 450, "no");
+addBookToLibrary('Harry Potter', "J. K. Rolling", 350, "yes");
+addBookToLibrary('Eragon', "Christopher Paoloni", 450, "yes");
+
+newBookForm = document.querySelector(".new-book");
+
+
+let showForm = false;
+
+function toggleForm() {
+
+        showForm = !showForm;
+        if(showForm){
+        newBookForm.style.display = "flex";
+        } else {
+        newBookForm.style.display = "none";
+        }
+}
+
+if (!showForm){
+    newBookForm.style.display = "none";
+}
+
+let newBookBtn = document.querySelector(".new-book-btn");
+newBookBtn.addEventListener("click", toggleForm);
+
 
 let cardsContainer = document.querySelector('.cards-container');
 
@@ -34,25 +57,35 @@ function showCards (){
 
         let bookAuthor = document.createElement('div');
         bookAuthor.className = "book-author";
-        bookAuthor.textContent = `${item.author}`;
+        bookAuthor.textContent = `author: ${item.author}`;
         book.appendChild(bookAuthor);
 
         let bookPages = document.createElement('div');
         bookPages.className = "book-pages";
-        bookPages.textContent = `${item.pages}`;
+        bookPages.textContent = `pages: ${item.pages}`;
         book.appendChild(bookPages);
+
+        let textRead = document.createElement('div');
+        textRead.className = "text-read";
+        textRead.textContent = "Already read?";
+        book.appendChild(textRead);
+
 
         let bookRead = document.createElement('button');
         bookRead.className = "book-check";
         bookRead.textContent = `${item.read}`;
         book.appendChild(bookRead);
         bookRead.addEventListener("click", ()=> {
-            if (bookRead.textContent == "yes"){
-                bookRead.textContent = "no";
+            if ( bookRead.textContent == "yes"){
+                item.read = "no";
+
             }
             else {
-                bookRead.textContent = "yes";
+
+                item.read = "yes";
             }
+            bookRead.textContent = item.read;
+            showCards();
                      });
             
                     
@@ -82,6 +115,11 @@ btn.addEventListener("click",
             read = 'yes';
         }else{
             read = 'no';
+        }
+        if (!title || !author || !pages){
+            alert("Please fill all the fields!");
+            showCards();
+            return;
         }
 
         addBookToLibrary(title,author,pages,read);
